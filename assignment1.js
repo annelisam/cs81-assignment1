0f6c9acb7bb8162947175136e4014abed70ba7df
Annelisa Moody <am@apeiron.io>
2019-09-16 15:33:36 -0700

    add prompt solution for HW

--- assignment1.js
+++ assignment1.js
@@ -6,3 +6,16 @@
 
 // total overall cost of the lease for 5 years
 
+const confirmReady = confirm("Are you ready to calculate your 5 year car lease?");
+
+
+if(confirmReady) {
+    const leaseAmount = prompt("What is the monthly leasing amount?");
+    let monthlyAmount = parseInt(leaseAmount);
+    alert("Your monthly lease amount is $" + monthlyAmount);
+    const increaseAmount = prompt("What is the yearly increase?");
+    let yearlyIncrease = parseInt(increaseAmount).toFixed(2);
+    alert("Your yearly increase amount is " + yearlyIncrease + "%");
+    let addIncrease = monthlyAmount*yearlyIncrease;
+    alert(addIncrease);
+}
